374e0854d79e8753f2e978db5f0f5a3f26484e53
Neil Baumann <accounts@iconicbrandgroup.com>
2026-07-29 13:35:44 -0400

    Retire legacy GoDaddy service worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+"use strict";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    Promise.all([
+      self.registration.unregister(),
+      caches.keys().then((keys) =>
+        Promise.all(keys.map((key) => caches.delete(key))),
+      ),
+    ])
+      .then(() =>
+        self.clients.matchAll({
+          includeUncontrolled: true,
+          type: "window",
+        }),
+      )
+      .then((clients) => {
+        for (const client of clients) {
+          client.navigate(client.url);
+        }
+      }),
+  );
+});
